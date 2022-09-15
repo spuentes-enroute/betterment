dada2edc1074f30936d643fe731b3a86410b4a55
spuentes-enroute <saul.puentes@enroutesystems.com>
2022-09-15 14:43:30 -0500

    CRUD: Edit and delete

--- ruby/projects/alpha_blog/app/controllers/articles_controller.rb
+++ ruby/projects/alpha_blog/app/controllers/articles_controller.rb
@@ -10,13 +10,37 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    # redirect_to article_path(@article)
-    redirect_to @article
+    if @article.save
+      # redirect_to article_path(@article)
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
--- ruby/projects/alpha_blog/config/routes.rb
+++ ruby/projects/alpha_blog/config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   # root 'application#hello'
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles , only: [:show, :index, :new, :create]
+  resources :articles
 end
