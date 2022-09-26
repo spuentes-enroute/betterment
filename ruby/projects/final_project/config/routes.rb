f4c61dedac1edadde6c38eb70ea38b01733cc656
spuentes-enroute <saul.puentes@enroutesystems.com>
2022-09-26 17:21:51 -0500

    Scope on views by role

--- ruby/projects/final_project/config/routes.rb
+++ ruby/projects/final_project/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  # get 'main/home'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#home"
+  # root "articles#index"
 end
