03cecfc2d1f9ac5330a295ac670c2649f4bb5acc
spuentes-enroute <saul.puentes@enroutesystems.com>
2022-10-05 15:26:02 -0500

    Adding user views and styling to alpha blog

--- ruby/projects/alpha_blog/app/controllers/users_controller.rb
+++ ruby/projects/alpha_blog/app/controllers/users_controller.rb
@@ -1,5 +1,43 @@
 class UsersController < ApplicationController
   def new
-   
+   @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account infromation was successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed in"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
--- ruby/projects/alpha_blog/config/routes.rb
+++ ruby/projects/alpha_blog/config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
